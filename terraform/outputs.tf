acaef5c05fcf7f5e0fc6e5b02f8c318e09c272a6
greyhoundforty <yantiffany@fastmail.com>
2021-08-31 08:33:11 -0500

    Added CLI example json
    Signed-off-by: greyhoundforty <yantiffany@fastmail.com>

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.id
+}
+
+output "consul_instance_ip" {
+  value = module.consul_cluster[*].primary_ipv4_address
+}
+
+output "bastion_public_ip" {
+  value = module.bastion.bastion_public_ip
+}
