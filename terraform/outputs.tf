a2d871c10b31b0acc635c1628ab24695e07ad0b6
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-09-10 13:32:26 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,6 @@ output "bastion_public_ip" {
   value = module.bastion.bastion_public_ip
 }
 
-output "rendered" {
-  value = data.template_file.init.rendered
-}
+# output "rendered" {
+#   value = data.template_file.init.rendered
+# }
